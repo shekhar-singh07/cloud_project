439a4709157f2d1ec051f4008b146da8d58c12d7
shekhar-singh07 <shekharchess8@gmail.com>
2026-06-19 17:25:18 +0530

    changed urls.py and settings

--- backend/notesproject/urls.py
+++ backend/notesproject/urls.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('notes.urls')),
     path('', TemplateView.as_view(template_name='index.html')),
 ]
+
+urlpatterns += static(
+    'assets/',
+    document_root=settings.BASE_DIR / 'dist' / 'assets'
+)
